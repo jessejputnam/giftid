4ce25ee6d5456aa08439a4cd2a81732230bbaa3b
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-12-17 15:46:04 -0500

    Add prefs to user

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const passport = require("passport");
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/user");
+
+// ######################################################
+// ######################################################
+
+exports.gift_preferences_get = (req, res, next) => {
+  return res.render("form-edit-pref");
+};
+
+exports.gift_preferences_post = async (req, res, next) => {
+  const new_pref = req.body.preference;
+  const prefs = req.user.gift_preferences;
+  prefs.push(new_pref);
+
+  try {
+    const user = await User.findById(req.user._id);
+    user.gift_preferences = prefs;
+    await user.save();
+
+    res.redirect("/home/gift-preferences");
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.gift_preferences_delete = async (req, res, next) => {
+  const i = req.body.index;
+  const prefs = req.user.gift_preferences;
+  prefs.splice(i, 1);
+  const user = await User.findById(req.user._id);
+  user.gift_preferences = prefs;
+  await user.save();
+
+  res.redirect("/home/gift-preferences");
+};
--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const auth_controller = require("../controllers/authController");
+const user_controller = require("../controllers/userController");
 
-/* GET users listing. */
+// GET request for user home
 router.get("/", auth_controller.home_get);
+
+// GET request for user gift preferences
+router.get("/gift-preferences", user_controller.gift_preferences_get);
+
+// POST request for add user gift preference
+router.post("/gift-preferences", user_controller.gift_preferences_post);
+
+// POST request for delete user gift prefernce
+router.post("/delete-preference", user_controller.gift_preferences_delete);
 
 module.exports = router;
